3fa061d6456ebc13f281a49f8e46c78f1473759a
Abhay Chauhan <abhay.11705360@lpu.in>
2025-06-25 18:08:09 +0530

    Corrected registration form

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -26,7 +26,6 @@ function Register() {
             <h2>Register</h2>
             <input name="fullName" placeholder="Full Name" onChange={handleChange} required />
             <input name="email" type="email" placeholder="Email" onChange={handleChange} required />
-            <input name="username" placeholder="Username" onChange={handleChange} required />
             <input name="password" type="password" placeholder="Password" onChange={handleChange} required />
             <button type="submit">Register</button>
         </form>
